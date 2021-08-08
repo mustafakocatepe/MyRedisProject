968e37d6488b201eddbd6b873971a0fb137d1ad2
Mustafa Said Kocatepe <msaidkocatepe@gmail.com>
2021-08-08 21:24:08 +0300

    --first commit

---
+++ Business/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using Core.Models;
+using Dashboard.Core.Caching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly IRedisCacheService _redisCacheService;
+
+        public ProductService(IRedisCacheService redisCacheService)
+        {
+            _redisCacheService = redisCacheService;
+        }
+        //public const string ProductDetail = "Category:{0}";
+        public const string ProductDetail = "Product:{0}";
+
+        public Product GetProductById(int Id)
+        {
+            //Check Redis            
+            var cacheKey = string.Format(ProductDetail, Id);
+            var result = _redisCacheService.Get<Product>(cacheKey);
+            //-------------------------------  
+
+            if (result != null)
+            {
+                return result;
+            }
+            else
+            {
+                var model = new Product()
+                {
+                    ID = 1,
+                    Name = "Nike Ayakkabi",
+                    SeriNo = "123"
+                };
+
+                _redisCacheService.Set(cacheKey, model);
+                return model;
+            }
+        }
+
+    }
+}
---
+++ Core/Models/Product.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Core.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SeriNo { get; set; }
+    }
+}
